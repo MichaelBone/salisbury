d60ec878a508f674cbadd2b813025a3cdfe9ed13
MichaelBone <37421067+MichaelBone@users.noreply.github.com>
2018-08-17 13:58:00 +0930

    Corrected variable name.

--- scraper.rb
+++ scraper.rb
@@ -62,7 +62,7 @@ while results_page
   count += 1
   puts "Parsing the results on page #{count}."
 
-  table = summary_page.root.at_css('.ContentPanel')
+  table = results_page.root.at_css('.ContentPanel')
   headers = table.css('th').collect { |th| th.inner_text.strip }
   development_applications += table.css('.ContentPanel, .AlternateContentPanel').collect do |tr| 
     tr.css('td').collect { |td| td.inner_text.strip }

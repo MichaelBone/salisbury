34ed0c468c6162774b9c62f9bea2e2938c22d1bc
MichaelBone <37421067+MichaelBone@users.noreply.github.com>
2018-08-17 14:10:45 +0930

    Changed to update the database.

--- scraper.rb
+++ scraper.rb
@@ -1,38 +1,10 @@
 require 'scraperwiki'
 require 'mechanize'
 
-agent = Mechanize.new
-
-def extract_from_p(contents, name)
-  contents.at('p:contains("' + name + '")').inner_text.split(name).last.strip
-end
-
-def scrape_page(page)
-  
-  # needs error checking particularly for date closing.
-  # what if parse fails.
-
-  contents = page.search('div.span6')
-  record = {
-    "info_url" => page.uri.to_s,
-    "council_reference" => contents.search('h1').inner_text.strip,
-    "comment_url" => page.uri.to_s,
-    "applicatant" => extract_from_p(contents,"Applicant:"),
-    "description" => contents.at('p:contains("Location:")').next_element.inner_text.strip,
-    "address" => extract_from_p(contents,"Location:"),
-    "date_scraped" => Date.today.to_s,
-    "date_closing" => Date.strptime(extract_from_p(contents,"Advertising closes "),"%d %b %Y"),
-  }
-
-  if (ScraperWiki.select("* from data where `council_reference`='#{record['council_reference']}'").empty? rescue true)
-    ScraperWiki.save_sqlite(['council_reference'], record)
-  else
-    puts "Skipping already saved record " + record['council_reference']
-  end
-end
-
 base_url = "https://eservices.salisbury.sa.gov.au/ePathway/Production/Web"
 comment_url = "mailto:city@salisbury.sa.gov.au"
+
+agent = Mechanize.new
 
 puts "Retrieving the default page."
 default_url = "#{base_url}/default.aspx"
@@ -57,14 +29,14 @@ button = enquiry_search_form.button_with(:value => "Search")
 results_page = agent.submit(enquiry_search_form, button)
 
 count = 0
-development_applications = []
+applications = []
 while results_page
   count += 1
   puts "Parsing the results on page #{count}."
 
   table = results_page.root.at_css('.ContentPanel')
   headers = table.css('th').collect { |th| th.inner_text.strip }
-  development_applications += table.css('.ContentPanel, .AlternateContentPanel').collect do |tr| 
+  applications += table.css('.ContentPanel, .AlternateContentPanel').collect do |tr| 
     tr.css('td').collect { |td| td.inner_text.strip }
   end
   
@@ -82,22 +54,34 @@ while results_page
   end
 end
 
-puts "Complete."
-  
-apps_links = []
+# Construct development application records that can be inserted into the database.
 
-links = enquiry_summary_view_page.search('a')
-links.each do |link|
-  break
-  href = link['href']
+application_records = applications.collect do |application|
+  application_record = {}
+  application_record['council_reference'] = application[headers.index('Application Number')]
+  application_record['info_url'] = "#{base_url}/default.aspx"
+  application_record['description'] = application[headers.index('Application Description')]
+  application_record['date_received'] = Date.strptime(application[headers.index('Lodgement Date')], '%d/%m/%Y').to_s
+  application_record['address'] = da_item[application.index('Site Address')]
+  application_record['date_scraped'] = Date.today.to_s
+  application_record['comment_url'] = comment_url
+  if application_record['description'].strip == ''
+    application_record['description'] = 'No description provided'
+  end
+  application
+end
 
-  if (href && href.start_with?("http://www.salisbury.sa.gov.au/Build/Planning_Building_and_Forms/Advertised_Development_Applications/") )
-    apps_links << href
+# Insert the records into the database.
+
+application_records.each do |application_record|
+  if application_record['council_reference'] != '' && application_record['address'] != ''  # avoid invalid records
+    ScraperWiki.save_sqlite(['council_reference'], application_record)
+    if (ScraperWiki.select("* from data where `council_reference`='#{application_record['council_reference']}'").empty? rescue true)
+      ScraperWiki.save_sqlite(['council_reference'], application_record)
+    else
+      puts "Skipping already saved record " + application_record['council_reference']
+    end
   end
 end
 
-apps_links.each_with_index do |app_url,index|
-  puts "Scraping application #{index} of #{apps_links.length} ..."
-  app = agent.get(app_url)
-  scrape_page(app)
-end
+puts "Complete."

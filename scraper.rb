fc1ae70b9ea239b1cb1c3f1c2ba1e81b6a64cd76
MichaelBone <37421067+MichaelBone@users.noreply.github.com>
2018-10-24 12:12:52 +1030

    Added tracing.

--- scraper.rb
+++ scraper.rb
@@ -14,7 +14,7 @@ default_page = agent.get(default_url + '?' + default_page.body.scan(/js=-?\d+/)[
 puts "Retrieving the enquiry lists page."
 puts default_page.body
 link = default_page.link_with(:href => '../Web/GeneralEnquiry/EnquiryLists.aspx')
-puts "Clicking link.:
+puts "Clicking link."
 enquiry_lists_page = link.click
 
 # The Date tab defaults to a search range of the last 30 days.
